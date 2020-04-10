395197a958d134d50106d58b61632e62fdb72c62
Tanaka <pyontana@gmail.com>
2020-04-10 16:54:37 +0900

    upd usre

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_secure_password         # passwordをハッシュ化する
+
+    validates :name, presence: true                         # 名前
+    validates :name_id, presence: true, uniqueness: true    # ログインID
 end
