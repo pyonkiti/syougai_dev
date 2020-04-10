2baa706509adcf0b3cfcd218c96478d850980473
Tanaka <pyontana@gmail.com>
2020-04-10 16:46:41 +0900

    upd controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'users/new'
-    get 'users/edit'
-    get 'users/show'
-    get 'users/index'
-  end
+    namespace :admin do
+        resources :users
+    end
+
     root to: 'tasks#index'
     resources :tasks
 
